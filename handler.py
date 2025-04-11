58e3da8b803e48b24b9971c20c463e26b598768f
Jon McConnell <87622525+jmcconne100@users.noreply.github.com>
2025-04-10 21:08:49 -0700

    Update handler.py

--- handler.py
+++ handler.py
@@ -2,7 +2,7 @@ import boto3
 import pandas as pd
 from faker import Faker
 import random
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 import os
 
 faker = Faker()
